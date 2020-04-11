e7ab84a22e8960144cb9242c088ca4034a94cf16
Dmitriy <zhirkod@gmail.com>
2020-04-11 12:49:47 +0300

    ADD APOD FUNCTIONALITY

---
+++ src/main/java/Parsing.java
@@ -0,0 +1,17 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Parsing {
+    private Object instance;
+    private String response;
+
+
+    public Parsing(Object instance, String response) {
+        this.instance = instance;
+        this.response = response;
+    }
+
+    public Object parseDataFromJson() throws JsonProcessingException {
+        return  new ObjectMapper().readValue(response, instance.getClass());
+    }
+}
---
+++ src/main/java/connection/APOD.java
@@ -0,0 +1,50 @@
+package connection;
+
+import okhttp3.HttpUrl;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
+public class APOD extends BaseNasa {
+    private String BaseAPODUrl;
+
+    public APOD() {
+        this.BaseAPODUrl = this.baseUrl + "planetary/apod";
+    }
+
+    public String run() throws IOException {
+        String url = createUrl();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
+    }
+
+    public String run(String date) throws IOException {
+        String url = createUrl(date);
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
+    }
+
+    private String createUrl(){
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(BaseAPODUrl).newBuilder();
+        urlBuilder.addQueryParameter("api_key", apiKey);
+        return urlBuilder.build().toString();
+    }
+
+    private String createUrl(String date){
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(BaseAPODUrl).newBuilder();
+        urlBuilder.addQueryParameter("api_key", apiKey);
+        urlBuilder.addQueryParameter("date", date);
+        return urlBuilder.build().toString();
+    }
+}
