7f723fb85aa842b932567845400949c20eb88ac6
Alexander Borovko <alexvonbor@gmail.com>
2020-04-15 08:59:09 +0300

    fill Techport

--- src/main/java/connection/Techport.java
+++ src/main/java/connection/Techport.java
@@ -1,6 +1,10 @@
 package connection;
 
 import okhttp3.HttpUrl;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
 
 public class Techport extends BaseNasa {
 
@@ -11,6 +15,13 @@ public class Techport extends BaseNasa {
         baseTechportURL = baseUrl + "techport/api/projects";
     }
 
+    public String run(String id) throws IOException {
+        techportURL = createURL(id);
+        Request request = new Request.Builder().url(techportURL).build();
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
+    }
 
     private String createURL(String idParametr){
         HttpUrl.Builder urlBuilder = HttpUrl.parse(baseTechportURL).newBuilder();
