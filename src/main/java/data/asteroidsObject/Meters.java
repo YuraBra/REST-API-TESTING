740facf46e8c7439b6d741c40bf71fcaab64104f
andrewkchn <andrewkchn@gmail.com>
2020-04-15 15:31:18 +0300

    Create asteroid query

---
+++ src/main/java/data/asteroidsObject/Meters.java
@@ -0,0 +1,16 @@
+
+package data.asteroidsObject;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class Meters {
+
+    private Double estimated_diameter_min;
+    private Double estimated_diameter_max;
+
+
+
+}
---
+++ src/main/java/data/asteroidsObject/Miles.java
@@ -0,0 +1,14 @@
+
+package data.asteroidsObject;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class Miles {
+
+    private Double estimated_diameter_min;
+    private Double estimated_diameter_max;
+
+}
