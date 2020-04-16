4ac52f3114f151b095ec1c0e0d62b64d5ed44d40
Alexander Borovko <alexvonbor@gmail.com>
2020-04-16 09:12:35 +0300

    fixes

--- src/main/java/data/techportObject/LibraryItems.java
+++ src/main/java/data/techportObject/LibraryItems.java
@@ -4,13 +4,13 @@ import java.util.List;
 
 public class LibraryItems {
 
-    private  Integer id;
-    private  String title;
-    private  String type;
-    private  Integer description;
-    private  String externalUrl;
-    private  String publishedBy;
-    private  String publishedDate;      // Integer ???
+    private Integer id;
+    private String title;
+    private String type;
+    private Integer description;
+    private String externalUrl;
+    private String publishedBy;
+    private String publishedDate;      // Integer ???
     private List<Files> files;
 
 
