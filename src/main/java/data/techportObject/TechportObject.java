3601787072c3e3dfa6ae93e11a542cebb624aa87
Alexander Borovko <alexvonbor@gmail.com>
2020-04-21 14:46:22 +0300

    fixes

--- src/main/java/data/techportObject/TechportObject.java
+++ src/main/java/data/techportObject/TechportObject.java
@@ -4,18 +4,19 @@ import java.util.List;
 
 public class TechportObject {
 
-    private  Integer id;
-    private  String lastUpdated;       // TODO change to date
-    private  String title;
-    private  String status;
-    private  String startDate;       // TODO change to date
-    private  String endDate;       // TODO change to date
-    private  String description;
-    private  String benefits;
-    private  String technologyMaturityStart;        // Integer ???
-    private  String technologyMaturityCurrent;        // Integer ???
-    private  String technologyMaturityEnd;        // Integer ???
-    private  String responsibleMissionDirectorateOrOffice;
+    private Integer id;
+    private String lastUpdated;       // TODO change to date
+    private String title;
+    private String status;
+    private String startDate;       // TODO change to date
+    private String endDate;       // TODO change to date
+    private String description;
+    private String benefits;
+    private String technologyMaturityStart;        // Integer ???
+    private String technologyMaturityCurrent;        // Integer ???
+    private String technologyMaturityEnd;        // Integer ???
+    private String responsibleProgram;
+    private String responsibleMissionDirectorateOrOffice;
     private List<LibraryItems> libraryItems;
     private List<String> closeoutDocuments;
     private List<SupportingOrganizations> supportingOrganizations;
