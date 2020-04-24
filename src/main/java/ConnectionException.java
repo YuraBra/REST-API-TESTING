4b5a698f05d25c173394ece9998c767096f107f8
Java <3809669@i.ua>
2020-04-24 17:15:22 +0300

    refactor

--- src/main/java/ConnectionException.java
+++
@@ -1,5 +0,0 @@
-public class ConnectionException extends RuntimeException {
-    public ConnectionException(String message) {
-        super(message);
-    }
-}
--- src/main/java/data/marsRoverPhotos/Cameras.java
+++
@@ -1,6 +0,0 @@
-package data.marsRoverPhotos;
-
-public class Cameras {
-    private String name;
-    private String full_name;
-}
