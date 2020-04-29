b4d9e263f758dfbdcee4dca1a486e129c0950991
Alexander Borovko <alexvonbor@gmail.com>
2020-04-29 22:44:57 +0300

    update

--- src/main/java/scooter/test/TestForUserData.java
+++ src/main/java/scooter/test/TestForUserData.java
@@ -20,13 +20,14 @@ public class TestForUserData {
     String activateToken;
     String bearerCode;
 
-    String email = "testforapi.h@mail.com";
+    String email = "testforapi.i@mail.com";
     String password = "qwerty";
-    String firstName = "Alex8";
-    String lastName = "Bor8";
+    String firstName = "Alex9";
+    String lastName = "Bor9";
+
     @Before
     public void createUser() throws IOException {
-        userDataDto = new UserDataDto(email,password,firstName,lastName);
+        userDataDto = new UserDataDto(email, password, firstName, lastName);
         SignUp signUp = new SignUp();
         activateToken = signUp.getResponse(userDataDto);
         System.out.println("Step 1");
@@ -41,12 +42,13 @@ public class TestForUserData {
         System.out.println("Step 3");
         System.out.println(bearerCode);
     }
-    private String getUserIdFromDecodedToken(String token){
+
+    private String getUserIdFromDecodedToken(String token) {
 
         String[] split_string = token.split("\\.");
         String base64EncodedBody = split_string[1];
         Base64 base64Url = new Base64(true);
-        System.out.println("~~~~~~~~~ JWT Body ~~~~~~~");
+        System.out.println("Step 4 decoded token");
         String body = new String(base64Url.decode(base64EncodedBody));
         System.out.println(body);
 
@@ -54,122 +56,46 @@ public class TestForUserData {
         int first = body.indexOf(firstSubStr);
         String secondSubStr = "\",\"role";
         int second = body.indexOf(secondSubStr);
-        String subStr = body.substring(first + firstSubStr.length(),second);
-        System.out.println("Step 4");
+        String subStr = body.substring(first + firstSubStr.length(), second);
+        System.out.println("Step 5");
         System.out.println(subStr);
         return subStr;
     }
 
+    private String getMail(String jsonAnswer){
+
+        String firstSubStr = "{\"email\":\"";
+        int first = jsonAnswer.indexOf(firstSubStr);
+        String secondSubStr = "\",\"firstN";
+        int second = jsonAnswer.indexOf(secondSubStr);
+        String subStr = jsonAnswer.substring(first + firstSubStr.length(), second);
+        System.out.println("Step 6");
+        System.out.println(subStr);
+        return subStr;
+    }
+
+    private String searchSubString(String str,String firstSubStr,String secondSubStr){
+        return null;
+    }
+
+
     @Test
-    public void getUserData(){
+    public void getUserData() {
         int actualStatus;
         String respSerID;
+        String actualMailFromJSON;
         String userId = getUserIdFromDecodedToken(bearerCode);
         UserData userData = new UserData(userId);
         try {
             respSerID = userData.run();
-            System.out.println("Step 5");
+            System.out.println("Step 7");
             System.out.println(respSerID);
-            Assert.assertEquals("String", respSerID);
+            actualMailFromJSON = getMail(respSerID);
+            Assert.assertEquals("String", actualMailFromJSON);
         } catch (IOException e) {
             Assert.fail();
         }
     }
 
-    /*int x;
-    int y;
-    @Before
-    public void setX(){
-        x =1;
-    }
-    @Test
-    public void setY(){
-       y=2;
-    }
-    @Test
-    public void sOut(){
-        System.out.println(x);
-        System.out.println(y);
-    }*/
-
-
-    /*UserDataDto userDataDto;
-    String activateToken;
-    String bearerCode;
-
-    String email = "testforapi.f@mail.com";
-    String password = "qwerty";
-    String firstName = "Alex5";
-    String lastName = "Bor5";
-
-    @Before
-    public void createUser(){
-        userDataDto = new UserDataDto(email,password,firstName,lastName);
-    }
-    @Test
-    public void setUpUser(){
-        SignUp signUp = new SignUp();
-
-        try{
-            activateToken = signUp.getResponse(userDataDto);
-            System.out.println("Step 1");
-            System.out.println(activateToken);
-        }catch (IOException e){ }                   // + Assert & Exception
-    }
-    @Test
-    public void activateUser(){
-        ActivateAccount acc = new ActivateAccount();
-        try{
-            //int actualCode = acc.run(activateToken.replace("\"", ""));
-            int actualCode = acc.run(activateToken.replace("\"", ""));
-            System.out.println("Step 2");
-            System.out.println(actualCode);
-        }catch (IOException e) { }                   // + Assert & Exception
-    }
-    @Test
-    public void singInUser(){
-        SignIn signIn = new SignIn();
-        try {
-            bearerCode = Objects.requireNonNull(signIn.getResponse(email, password)
-                    .header("Authorization")).replace("Bearer ", "");
-            System.out.println("Step 3");
-            System.out.println(bearerCode);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    // сделан
-    private String getUserIdFromDecodedToken(String token){
-
-        String[] split_string = token.split("\\.");
-        String base64EncodedBody = split_string[1];
-        Base64 base64Url = new Base64(true);
-        System.out.println("~~~~~~~~~ JWT Body ~~~~~~~");
-        String body = new String(base64Url.decode(base64EncodedBody));
-        System.out.println(body);
-
-        String firstSubStr = "{\"sub\":\"";
-        int first = body.indexOf(firstSubStr);
-        String secondSubStr = "\",\"role";
-        int second = body.indexOf(secondSubStr);
-        String subStr = body.substring(first + firstSubStr.length(),second);
-        System.out.println("Step 4");
-        System.out.println(subStr);
-        return subStr;
-    }
-
-    @Test
-    public void getUserData(){
-        int actualStatus;
-        String userId = getUserIdFromDecodedToken(bearerCode);
-        UserData userData = new UserData(userId);
-        try {
-            actualStatus = userData.run();
-            System.out.println("Step 5");
-            System.out.println(actualStatus);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }*/
 
 }
