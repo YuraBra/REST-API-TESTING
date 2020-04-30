6fa943f407c4dd3e0411b5fa80f68de21fa1f426
Alexander Borovko <alexvonbor@gmail.com>
2020-04-30 06:47:05 +0300

    fixes

--- src/main/java/nasa/testData/TechPortData.java
+++ src/main/java/nasa/testData/TechPortData.java
@@ -10,23 +10,23 @@ public class TechPortData {
     public static final String id3 = "17756";
     public static final String id4 = "17759";
 
-    public static final String title1 = "Integration of Tactical Departure Scheduling and Traffic Flow Management, Phase I";
-    public static final String title2 = "Improved hyperspectral imaging technologies, Phase I";
-    public static final String title3 = "ISS Launched Cubesat Demonstration of Variable-Drag Magnetoshell Aerocapture, Phase I";
-    public static final String title4 = "Robust Voice Communication Understanding for Single-Pilot Operations, Phase I";
+    private String title1 = "Integration of Tactical Departure Scheduling and Traffic Flow Management, Phase I";
+    private String title2 = "Improved hyperspectral imaging technologies, Phase I";
+    private String title3 = "ISS Launched Cubesat Demonstration of Variable-Drag Magnetoshell Aerocapture, Phase I";
+    private String title4 = "Robust Voice Communication Understanding for Single-Pilot Operations, Phase I";
 
-    HashMap<String, String> myHashMap;
+    HashMap<String, String> mapTitles;
 
     public TechPortData() {
-        myHashMap = new HashMap<String, String>();
-        myHashMap.put(id1,title1);
-        myHashMap.put(id2,title2);
-        myHashMap.put(id3,title3);
-        myHashMap.put(id4,title4);
+        mapTitles = new HashMap<String, String>();
+        mapTitles.put(id1,title1);
+        mapTitles.put(id2,title2);
+        mapTitles.put(id3,title3);
+        mapTitles.put(id4,title4);
 
     }
 
     public String getTitle(String id){
-        return myHashMap.get(id);
+        return mapTitles.get(id);
     }
 }
