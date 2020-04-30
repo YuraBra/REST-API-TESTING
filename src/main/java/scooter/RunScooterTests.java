6b88c713caefae4feaa69cbafb4599639d040970
Nezhenceva <anna.nezhenceva@ukr.net>
2020-04-30 11:18:24 +0300

    Create TestForUserSolvency

--- src/main/java/scooter/RunScooterTests.java
+++ src/main/java/scooter/RunScooterTests.java
@@ -3,17 +3,15 @@ package scooter;
 import org.junit.experimental.categories.Categories;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import scooter.test.TestForActivateAccount;
-import scooter.test.TestForAuthorizedUser;
-import scooter.test.TestForNewUser;
-import scooter.test.TestScooters;
+import scooter.test.*;
 
 @RunWith(Categories.class)
 @Suite.SuiteClasses({
         TestScooters.class,
         TestForNewUser.class,
         TestForActivateAccount.class,
-        TestForAuthorizedUser.class
+        TestForAuthorizedUser.class,
+        TestForUserSolvency.class
 })
 public class RunScooterTests {
 }
