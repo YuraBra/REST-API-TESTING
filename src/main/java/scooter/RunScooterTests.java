1b6a0ab2f6696163b37ae25e721dc9a1c7d5684c
PavloY <pavlo.yakovlev@gmail.com>
2020-04-30 10:16:23 +0300

    Update RunScooterTests.java

--- src/main/java/scooter/RunScooterTests.java
+++ src/main/java/scooter/RunScooterTests.java
@@ -3,11 +3,13 @@ package scooter;
 import org.junit.experimental.categories.Categories;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+import scooter.test.TestScooterStatus;
 import scooter.test.TestScooters;
 
 @RunWith(Categories.class)
 @Suite.SuiteClasses({
-        TestScooters.class
+        TestScooters.class,
+        TestScooterStatus.class
 })
 public class RunScooterTests {
 }
